3f7c368816c31246c14ab555d30365e081bad828
Abdalaziz Mohamed <89922655+amohamedhey@users.noreply.github.com>
2024-04-15 12:25:18 +0200

    feat: comment on PR using unique ID (#1)

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,15 @@
-ARG ALPINE_VERSION="3.19"
+ARG BASE_RUNNER_IMAGE_VERSION="1.3.0"
 
-ARG BASH_VERSION="5.2.26-r0"
-
-FROM alpine:${ALPINE_VERSION}
+FROM heycardocker/infra-docker-actions:${BASE_RUNNER_IMAGE_VERSION}
 
 WORKDIR /scripts
-
-RUN apk update --no-cache; \
-    apk upgrade --no-cache; \
-    apk add --no-cache bash=${BASH_VERSION}; \
-    rm -rf /var/cache/apk/*
 
 ENV LOG_LEVEL "INFO"
 ENV LOG_TIMESTAMPED "true"
 ENV DEBUG_MODE "false"
 
 COPY scripts/utils.sh .
+COPY scripts/gh-utils.sh .
 COPY scripts/script.sh .
 
 ENTRYPOINT ["/scripts/script.sh"]
---
+++ scripts/gh-utils.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+function get_formatted_comment_id() {
+  _comment_id="${1}"
+  echo "<!-- comment-id:${_comment_id} -->"
+}
+
+# The `delete_previous_comments` function deletes comments by their ids from pull requests.
+function delete_previous_comments() {
+  _repo_org="${1}"
+  _repo_name="${2}"
+  _pr_number="${3}"
+
+  _nextPage="1"
+  while [[ "${_nextPage}" != "0" ]]; do
+    _comments="$(gh api "/repos/${_repo_org}/${_repo_name}/issues/${_pr_number}/comments?direction=asc&per_page=20&page=${_nextPage}")"
+    if [[ "$(cat "${_comments}" | jq '.|length')" == 0 ]]; then
+      _nextPage="0"
+    else
+      _nextPage="$((_nextPage + 1))"
+    fi
+    while read -r _previous_comment_id; do
+      log_out "Deleting previous comment with ID: ${_previous_comment_id}"
+      gh api "/repos/${_repo_org}/${_repo_name}/issues/comments/${_previous_comment_id}" -X DELETE >/dev/null
+    done < <(echo "${_comments}" | jq ".[] | select(.body|startswith(\"$(get_formatted_comment_id "${_comment_id}")\")) | .id")
+  done
+}
+
+# The `comment_on_pull_request` function pushes a comment to a pull request.
+function comment_on_pull_request() {
+  _repo_org="${1}"
+  _repo_name="${2}"
+  _pr_number="${3}"
+  _comment_body="${4}"
+  _delete_previous_comments="${5}"
+  _comment_id="${6}"
+
+  if [[ "$(check_bool "${_delete_previous_comments}")" ]]; then
+    if [[ -z "${_comment_id}" ]]; then
+      log_out "No comment id was provided for deleting previous comments. Aborting." "FATAL" 1
+    else
+      delete_previous_comments "${_repo_org}" "${_repo_name}" "${_pr_number}"
+    fi
+  fi
+
+  log_out "Commenting on ${_repo_org}/${_repo_name}#${_pr_number}"
+  if [[ -z "${_comment_id}" ]]; then
+    printf "%s" "$(echo "${_comment_body}")" | gh pr comment "${_pr_number}" -R "${_repo_org}/${_repo_name}" -F -
+  else
+    printf "%s \n %s" "$(get_formatted_comment_id "${_comment_id}")" "$(cat "${_comment_body}")" | gh pr comment "${_pr_number}" -R "${_repo_org}/${_repo_name}" -F -
+  fi
+}
